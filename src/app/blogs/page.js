24f2939771afc98dc880387a758f8422ea2438c9
abdul-sami-zelle <abdulsami.zellesolutions@gmail.com>
2026-06-23 21:12:51 +0500

    suspense issue

--- src/app/blogs/page.js
+++ src/app/blogs/page.js
@@ -1,3 +1,40 @@
+// import BlogPage from './BlogPage';
+
+
+// export async function generateMetadata() {
+//   return {
+//     title: "Furniture Mecca's Blogs | Furniture Sale – Save Up to 75% Furniture Mecca",
+//     description:
+//       "Explore Furniture Mecca's blog for furniture tips, home décor inspiration, design trends, seasonal ideas, and expert advice for every room.",
+
+//     alternates: {
+//       canonical: "https://myfurnituremecca.com/blogs",
+//     },
+
+//     openGraph: {
+//       title: "Furniture Mecca's Blogs | Furniture Sale – Save Up to 75% Furniture Mecca",
+//       description:
+//         "Explore Furniture Mecca's blog for furniture tips, home décor inspiration, design trends, seasonal ideas, and expert advice for every room.",
+//       url: "https://myfurnituremecca.com/blogs",
+//       images: [
+//         {
+//           url: "https://myfurnituremecca.com/Assets/Logo/fm-new-logo.png",
+//           width: 1200,
+//           height: 630,
+//         },
+//       ],
+//     },
+//   };
+// }
+
+// const Blogs = () => {
+//   return <BlogPage />;
+// };
+
+// export default Blogs;
+
+
+import { Suspense } from 'react';
 import BlogPage from './BlogPage';
 
 export async function generateMetadata() {
@@ -5,11 +42,9 @@ export async function generateMetadata() {
     title: "Furniture Mecca's Blogs | Furniture Sale – Save Up to 75% Furniture Mecca",
     description:
       "Explore Furniture Mecca's blog for furniture tips, home décor inspiration, design trends, seasonal ideas, and expert advice for every room.",
-
     alternates: {
       canonical: "https://myfurnituremecca.com/blogs",
     },
-
     openGraph: {
       title: "Furniture Mecca's Blogs | Furniture Sale – Save Up to 75% Furniture Mecca",
       description:
@@ -27,7 +62,11 @@ export async function generateMetadata() {
 }
 
 const Blogs = () => {
-  return <BlogPage />;
+  return (
+    <Suspense fallback={null}>
+      <BlogPage />
+    </Suspense>
+  );
 };
 
 export default Blogs;
